2c3e2514194ec7a067aa5d08318eefeb1611b7c9
BuddyL <buddy@buddylindsey.com>
2012-02-20 19:59:50 -0600

    get structure of the wsgihandler

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is a project for trying to re-create the `very basics` of Django to better learn how it works internally. Part of the code comes from _learning_ how to handle what the low level stuff is/does, and the other comes from digging into _how_ django works to better demonstrate it.
 
+## Note (Not a Production App)
+This is for example and learning purposes. It is not meant to be an alternative to Django or even used in any production model ever.
--- environment.py
+++ environment.py
@@ -1,16 +1,14 @@
 from wsgiref.simple_server import make_server
 
 def application(environ, start_response):
-    response_body = ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
-    response_body = '\n'.join(response_body)
-
     status = '200 OK'
+    output = 'Hello World!'
 
     response_headers = [('Content-Type', 'text/plain'),
-                        ('Content-Length', str(len(response_body)))]
+                        ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
 
-    return [response_body]
+    return [output]
 
-httpd = make_server('localhost', 8051, application)
-httpd.handle_request()
+httpd = make_server('localhost', 8000, application)
+httpd.serve_forever()
